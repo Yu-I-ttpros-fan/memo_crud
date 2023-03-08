6ec5f1b49d7f28ae4747ef16e785d5fe169cdde3
yu-i-dimasfan <liaang19hgr3@gmail.com>
2023-03-09 03:16:00 +0900

    トップページ、omniauthの設定を作成

---
+++ memo_app/db/migrate/20230308175243_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :provider, null: false
+      t.string :uid, null: false
+      t.string :name, null: false
+      t.string :image_url, null: false
+
+      t.timestamps
+    end
+
+    # 検索しやすいようにインデックスをprovider,uidに対して張る（並び替えたり）
+    add_index :users, %i[provider uid], unique: true
+  end
+end
