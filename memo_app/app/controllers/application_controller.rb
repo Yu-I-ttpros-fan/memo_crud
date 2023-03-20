c4ebc2f7ddd63390f728af9e0d72404791222b3f
yu-i-dimasfan <liaang19hgr3@gmail.com>
2023-03-20 23:40:35 +0900

    ログインしている状態でメモ機能が触れるように処理を追加 レイアウトを変更

--- memo_app/app/controllers/application_controller.rb
+++ memo_app/app/controllers/application_controller.rb
@@ -1,10 +1,25 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  before_action :authenticate
   helper_method :signed_in?
 
   private
 
   def signed_in?
     !!session[:user_id]
+  end
+
+  def current_user
+    # user_idがセッションに格納されており、
+    # @current_userに何も入ってなかったら@current_userに格納する
+    return unless session[:user_id]
+
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def authenticate
+    # サインインしていれば処理をやめ、していなければログインを促す
+    return if signed_in?
+
+    redirect_to root_path, alert: 'サインインしてください'
   end
 end
--- memo_app/app/controllers/sessions_controller.rb
+++ memo_app/app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
+  skip_before_action :authenticate, only: :create
   def create
     # userモデルから得たユーザ情報をセッションに格納
     # user情報を得るためにauthの情報をuserモデルに渡す
     user = User.find_or_create_from_auth_hash!(request.env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to root_path, notice: 'サインインに成功しました'
+    redirect_to memos_path, notice: 'サインインに成功しました'
   end
 
   def destroy
