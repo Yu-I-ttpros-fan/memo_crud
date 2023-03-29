4910e554425d498ccf5281dc0f05c6a8592b5ccd
yu-i-dimasfan <liaang19hgr3@gmail.com>
2023-03-30 03:53:07 +0900

    変種機能作成、リファクタリング、フォームを部分テンプレートに切り出し、バリデーション失敗時の挙動を別メソッドに切り出し

--- memo_app/app/controllers/memos_controller.rb
+++ memo_app/app/controllers/memos_controller.rb
@@ -1,20 +1,33 @@
 class MemosController < ApplicationController
   def index
     # ユーザごとにメモ一覧を表示させる
-    @memos = current_user.memos.all.order(created_at: :desc)
+    @memos = current_user.created_memos.all.order(created_at: :desc)
   end
 
   def new
-    @memo = current_user.create_memos.build
+    @memo = current_user.created_memos.build
   end
 
   def create
-    @memo = current_user.create_memos.build(memo_params)
+    @memo = current_user.created_memos.build(memo_params)
 
     if @memo.save
       redirect_to memos_path, notice: '新規メモを登録しました'
     else
-      render :new, status: :unprocessable_entity
+      failure_save
+    end
+  end
+
+  def edit
+    @memo = current_user.created_memos.find(params[:id])
+  end
+
+  def update
+    @memo = current_user.created_memos.find(params[:id])
+    if @memo.update(memo_params)
+      redirect_to memos_path, notice: 'メモを更新しました'
+    else
+      failure_save
     end
   end
 
@@ -24,5 +37,9 @@ class MemosController < ApplicationController
     params.require(:memo).permit(
       :title, :content
     )
+  end
+
+  def failure_save
+    render :new, status: :unprocessable_entity
   end
 end
--- memo_app/app/models/user.rb
+++ memo_app/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :create_memos, class_name: 'Memo'
-  has_many :memos
+  has_many :created_memos, class_name: 'Memo'
+
   def self.find_or_create_from_auth_hash!(auth_hash)
     recieved_provider = auth_hash[:provider]
     recieved_uid = auth_hash[:uid]
