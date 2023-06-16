690f2ae89923ebf80a73daec8a9a68a9aebd9b30
yu-i-dimasfan <liaang19hgr3@gmail.com>
2023-06-17 03:15:58 +0900

    ジャンルを選択できるようにしました

--- memo_app/app/controllers/memos_controller.rb
+++ memo_app/app/controllers/memos_controller.rb
@@ -10,6 +10,7 @@ class MemosController < ApplicationController
 
   def create
     @memo = current_user.created_memos.build(memo_params)
+    set_genre_id_from_params
 
     if @memo.save
       redirect_to memos_path, notice: '新規メモを登録しました'
@@ -20,10 +21,12 @@ class MemosController < ApplicationController
 
   def edit
     @memo = current_user.created_memos.find(params[:id])
+    @selected_genre_id = @memo.genre_id
   end
 
   def update
     @memo = current_user.created_memos.find(params[:id])
+    set_genre_id_from_params
     if @memo.update(memo_params)
       redirect_to memos_path, notice: 'メモを更新しました'
     else
@@ -41,11 +44,15 @@ class MemosController < ApplicationController
 
   def memo_params
     params.require(:memo).permit(
-      :title, :content
+      :title, :content, :genre_id
     )
   end
 
   def failure_save
     render :new, status: :unprocessable_entity
+  end
+
+  def set_genre_id_from_params
+    @memo.genre_id = params[:memo][:genre_id] if params[:memo][:genre_id].present?
   end
 end
