6ff484bafe866f6c7e3b388e2f98e3bb282dcb98
yu-i-dimasfan <liaang19hgr3@gmail.com>
2023-10-09 20:11:05 +0900

    google_oauth2でログインしたユーザのテストデータを準備できるように処理を追加・修正しました。

--- memo_app/test/factories/memos.rb
+++ memo_app/test/factories/memos.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     association :user, factory: :user # ファクトリーを指定して関連付け
     sequence(:title) { |i| "タイトル#{i}" }
     sequence(:content) { |i| "内容#{i}" }
+
+    trait :google_oauth2 do
+      association :user, factory: [:user, :google_oauth2]
+      sequence(:title) { |i| "タイトル#{i}" }
+      sequence(:content) { |i| "内容#{i}" }
+    end
   end
 end
