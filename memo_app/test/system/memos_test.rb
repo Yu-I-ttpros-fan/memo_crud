a296aa0b553f041286ade85cdda832d5a0fac0cf
yu-i-dimasfan <liaang19hgr3@gmail.com>
2023-10-24 03:09:19 +0900

    メモ登録時のテストを書きました

--- memo_app/test/system/memos_test.rb
+++ memo_app/test/system/memos_test.rb
@@ -16,4 +16,21 @@ class MemosTest < ApplicationSystemTestCase
 
     assert_selector 'h1', text: 'メモ一覧'
   end
+
+  test 'メモ登録ページ 記入して登録' do
+    # github認証テストがあるためsetupメソッドを使わずに記載した
+    user = FactoryBot.create(:user, :google_oauth2)
+    sign_in_google_auth(user)
+
+  visit new_memo_url
+  assert_selector "h1", text: 'メモ登録ページ'
+
+  fill_in "タイトル", with: "タイトル"
+  fill_in "メモ", with: "メモテスト"
+
+  click_on "メモを登録する"
+  assert_selector "div.alert", text: "新規メモを登録しました"
+  end
+
+
 end
