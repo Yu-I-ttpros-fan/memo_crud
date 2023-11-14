caecde4d8aa68cb88958dadca566ce6f0d5fde88
yu-i-dimasfan <liaang19hgr3@gmail.com>
2023-11-15 01:09:24 +0900

    controllerテスト勉強用に作成

--- memo_app/test/sign_in_helper.rb
+++ memo_app/test/sign_in_helper.rb
@@ -13,10 +13,18 @@ module SignInHelper
   })
 
   #作成したデータをもとにサインインテスト
+  case 
+  when respond_to?(:visit)
     visit root_url
     click_on 'GitHubでサインイン'
-    @current_user = user
+  when respond_to?(:get)
+    get '/auth/github/callback'
+  else
+    raise NotImplementedError.new
   end
+  @current_user = user
+  end
+
 
   def sign_in_google_auth(user)
     OmniAuth.config.test_mode = true
@@ -30,12 +38,23 @@ module SignInHelper
     })
 
     #作成したデータをもとにサインインテスト
-    visit root_url
-    click_on 'Googleでサインイン'
+    case 
+    when respond_to?(:visit)
+      visit root_url
+      click_on 'Googleでサインイン'
+    when respond_to?(:get)
+      get '/auth/google_oauth2/callback'
+    else
+      raise NotImplementedError.new
+    end
     @current_user = user
   end
 
   def current_user
     @current_user
+  end
+
+  class ActionDispatch::IntegrationTest
+    include SignInHelper
   end
 end
