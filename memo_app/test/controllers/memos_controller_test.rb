74c5015799b119b88a3cccafe13831f46277ce82
yu-i-dimasfan <liaang19hgr3@gmail.com>
2023-11-24 03:04:04 +0900

    メモ一覧表示に関するテスト

--- memo_app/test/controllers/memos_controller_test.rb
+++ memo_app/test/controllers/memos_controller_test.rb
@@ -1,6 +1,33 @@
 require "test_helper"
 
 class MemosControllerTest < ActionDispatch::IntegrationTest
+   test "メモ一覧を表示する" do
+      test_user = FactoryBot.create(:user, :google_oauth2)
+      # ユーザ分、複数のメモを作成する
+      FactoryBot.create_list(:memo, 3, user: test_user)
+
+      sign_in_google_auth test_user
+      assert_response :redirect
+      get memos_url
+      assert_select 'div.card.mb-2', count: 3
+   end
+
+   test "ほかのユーザのメモは表示されない" do
+            test_user = FactoryBot.create(:user, :google_oauth2)
+      # ユーザ分、複数のメモを作成する
+      FactoryBot.create_list(:memo, 3, user: test_user)
+      sign_in_user = FactoryBot.create(:user, :google_oauth2)
+
+      sign_in_google_auth sign_in_user
+      assert_response :redirect
+      get memos_url
+      assert_select 'div.card.mb-2', count: 0
+   end
+
+   test "他のプロバイダでログインしたメモは表示されない" do
+
+   end
+
    test "自分が作成したメモを削除" do
      test_user = FactoryBot.create(:user, :google_oauth2 )
      memo = FactoryBot.create(:memo, user: test_user)
