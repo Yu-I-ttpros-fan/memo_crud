40a3ccc06718a1ff5909d7c1b76b8a07f4dbbf54
yu-i-dimasfan <liaang19hgr3@gmail.com>
2023-11-29 02:47:13 +0900

    modelのテスト用兼ビジネスロジックを切り出すための変更

--- memo_app/app/controllers/memos_controller.rb
+++ memo_app/app/controllers/memos_controller.rb
@@ -1,6 +1,6 @@
 class MemosController < ApplicationController
   def index
-    @memos = memo_display
+    @memos = Memo.memo_display(params,current_user)
     @genres = Genre.all
     @selected_genre_id = params[:genre_id].presence
   end
@@ -56,19 +56,5 @@ class MemosController < ApplicationController
   def set_genre_id_from_params
     # 編集、登録画面でのジャンルの登録
     @memo.genre_id = params[:memo][:genre_id] if params[:memo][:genre_id].present?
-  end
-
-  def memo_display
-    # 表示しやすいように機能を追加しやすいように処理を切り出し
-    # 並び替えの処理
-    order_rule = params[:order_rule].present? ? params[:order_rule] : 'created_at'
-    order_time = params[:order_time].present? ? params[:order_by] : 'desc'
-
-    memos = current_user.created_memos.order("#{order_rule} #{order_time}")
-
-    # ジャンルで表示数を絞る
-    memos = memos.where(genre_id: params[:genre_id]).order("#{order_rule} #{order_time}") if params[:genre_id].present?
-
-    memos
   end
 end
--- memo_app/app/models/memo.rb
+++ memo_app/app/models/memo.rb
@@ -2,4 +2,18 @@ class Memo < ApplicationRecord
   belongs_to :user
   validates :title, length: { maximum: 50 }, presence: true
   validates :content, length: { maximum: 500 }, presence: true
+
+  def self.memo_display(params,current_user)
+    # 表示しやすいように機能を追加しやすいように処理を切り出し
+    # 並び替えの処理
+    order_rule = params[:order_rule].present? ? params[:order_rule] : 'created_at'
+    order_time = params[:order_time].present? ? params[:order_by] : 'desc'
+
+    memos = current_user.created_memos.order("#{order_rule} #{order_time}")
+
+    # ジャンルで表示数を絞る
+    memos = memos.where(genre_id: params[:genre_id]).order("#{order_rule} #{order_time}") if params[:genre_id].present?
+
+    memos
+  end
 end
