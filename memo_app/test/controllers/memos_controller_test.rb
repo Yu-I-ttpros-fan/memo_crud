1523687b67d66b6c9b01a78056fa8a2b5e0200d8
yu-i-dimasfan <liaang19hgr3@gmail.com>
2023-11-29 02:43:42 +0900

    テスト加筆

--- memo_app/test/controllers/memos_controller_test.rb
+++ memo_app/test/controllers/memos_controller_test.rb
@@ -13,7 +13,7 @@ class MemosControllerTest < ActionDispatch::IntegrationTest
    end
 
    test "ほかのユーザのメモは表示されない" do
-            test_user = FactoryBot.create(:user, :google_oauth2)
+      test_user = FactoryBot.create(:user, :google_oauth2)
       # ユーザ分、複数のメモを作成する
       FactoryBot.create_list(:memo, 3, user: test_user)
       sign_in_user = FactoryBot.create(:user, :google_oauth2)
@@ -25,7 +25,14 @@ class MemosControllerTest < ActionDispatch::IntegrationTest
    end
 
    test "他のプロバイダでログインしたメモは表示されない" do
+      test_user = FactoryBot.create(:user, :google_oauth2)
+      # ユーザ分、複数のメモを作成する
+      FactoryBot.create_list(:memo, 3, user: test_user)
 
+      sign_in_github test_user
+      assert_response :redirect
+      get memos_url
+      assert_select 'div.card.mb-2', count: 0
    end
 
    test "自分が作成したメモを削除" do
