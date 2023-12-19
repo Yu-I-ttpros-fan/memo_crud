d005a8ad6703b589575fa8572184b86c83af8577
yu-i-dimasfan <liaang19hgr3@gmail.com>
2023-12-20 02:52:04 +0900

    ジャンル取得テスト

--- memo_app/test/models/genre_test.rb
+++ memo_app/test/models/genre_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class GenreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "指定されたジャンルが取得できること" do
+    @genre = FactoryBot.create(:genre)
+    assert_equal 'ジャンル1', @genre.name
+  end
 end
